598df9d18be0402439d1396bc94b71b49e247c4b
AyushAJS <jangidayush89@gmail.com>
2025-12-07 10:33:01 +0530

    some methods for token and password is added in the utils

---
+++ src/utils/auth.utils.ts
@@ -0,0 +1,32 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const getPasswordHash = async (plainTextPassword: string)=> {
+    const hashedPassword = await bcrypt.hash(plainTextPassword, 10);
+
+    return hashedPassword;
+}
+
+export const validatePassword = async (plainTextPassword: string, hashedPassword: string)=> {
+    const isValidPassword = await bcrypt.compare(plainTextPassword, hashedPassword);
+
+    return isValidPassword;
+}
+
+export const getJWT = async (id: string, email: string)=> {
+    const secretKey = process.env.JWT_SECRET as string;
+    const jwtToken = jwt.sign({ id, email }, secretKey, { expiresIn: '7d' });
+
+    return jwtToken;
+}
+
+
+export const validToken = async (token: string)=> {
+    const secretKey = process.env.JWT_SECRET as string;
+    const decoded = jwt.verify(token, secretKey);
+
+    return {
+        id: decoded?.id as string,
+        email: decoded?.email as string
+    }
+}
