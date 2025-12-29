8911cd4fe4ef9a894d7a29617436db0c21b13f00
AyushAJS <jangidayush89@gmail.com>
2025-12-29 17:40:39 +0530

    embedding text method updation use gemini instead of openAI api to resolve the limit msg

--- src/utils/embeddings.utils.ts
+++ src/utils/embeddings.utils.ts
@@ -1,21 +1,16 @@
-import OpenAI from "openai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+const model = genAI.getGenerativeModel({ model: "text-embedding-004" });
 
-const client = new OpenAI({
-    apiKey: process.env.OPEN_AI_API_KEY
-});
-
-const getEmbeddedText = async (text: string): Promise<number[]>=> {
-    try {
-        const response = await client.embeddings.create({
-            model: "text-embedding-3-small", // cheap + good
-            input: text,
-          });
-        
-          return (response as any).data[0].embedding;
-    }catch(err) {
-        throw new Error("Error in text embedding");
-    }
-}
+const getEmbeddedText = async (text: string): Promise<number[]> => {
+  try {
+    const result = await model.embedContent(text);
+    return result.embedding.values;
+  } catch (err: any) {
+    console.error("Gemini embedding error: ", err);
+    throw new Error("Error in text embedding");
+  }
+};
 
 export default getEmbeddedText;
